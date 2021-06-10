94e5eaf1f5f6c9183dee8dfc49a5b0c9b9e9643b
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-06-10 20:48:55 +0200

    make query on mariadb database

--- punchjob.js
+++ punchjob.js
@@ -1,11 +1,12 @@
 
-const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler')
-console.log('Yah, vamos ver');
+import { ToadScheduler, SimpleIntervalJob, Task } from 'toad-scheduler'
+
+console.log('Estamos a trabalhar')
 
 const scheduler = new ToadScheduler()
 
-const task = new Task('simple task', () => { 
-console.log('Corri o job')
+const task = new Task('simple task', () => {
+  console.log('Corri o job')
 })
 const job = new SimpleIntervalJob({ seconds: 20, }, task)
 
--- punchschedulejob.js
+++ punchschedulejob.js
@@ -1,7 +1,44 @@
 
 import schedule from 'node-schedule';
+import mariadb from './database/mariadb.js'
 
 console.log('Estamos a trabalhar')
+
+async function jobCopyUsers() {
+  let conn;
+  try {
+    conn = await mariadb.getConnection();
+    await conn.query('Call jobCopyUsers');
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release(); //release to pool
+  }
+}
+
+async function jobCopyUserGroups() {
+  let conn;
+  try {
+    conn = await mariadb.getConnection();
+    await conn.query('Call jobCopyUserGroups');
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release(); //release to pool
+  }
+}
+
+async function jobCopyPunchLog() {
+  let conn;
+  try {
+    conn = await mariadb.getConnection();
+    await conn.query('Call jobCopyPunchLog');
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) conn.release(); //release to pool
+  }
+}
 
 
 //Correr o job numa data especifica
@@ -12,6 +49,12 @@ console.log('Estamos a trabalhar')
 // })
 
 //correr o job de 3 em 3 segundos
-schedule.scheduleJob('*/3 * * * * *', ()=>{
-    console.log('Corri o job')
+schedule.scheduleJob('*/10 * * * * *', async ()=>{
+    console.log('Inicio do job')
+
+    await jobCopyUserGroups()
+    await jobCopyUsers()
+    await jobCopyPunchLog()
+
+    console.log('Fim do job')
 })
